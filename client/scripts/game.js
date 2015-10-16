a6bc7892748b91585f0fc1014a81e0448448d0ff
Sanjay Chakrapani <csanjay@gmail.com>
2015-10-15 20:14:02 -0400

    (refactor) moved routing information to within individual module

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -1,15 +1,4 @@
 angular.module('app', [
   'ui.router',
   'app.intro'
-])
-
-.config(['$stateProvider',
-  function($stateProvider) {
-    $stateProvider
-      .state('intro', {
-        url: '',
-        templateUrl: 'templates/intro.html',
-        controller: 'IntroController'
-      });
-  }
 ]);
