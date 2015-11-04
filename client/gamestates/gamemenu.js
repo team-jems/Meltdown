242d0c4134d9cef2437623942d1852d638a688cc
Mike Hsieh <mythsieh@gmail.com>
2015-11-04 12:10:38 -0800

    Refactored game menu to contain no options

--- client/gamestates/gamemenu.js
+++ client/gamestates/gamemenu.js
@@ -5,18 +5,18 @@ var GameMenu = function(game) {};
 GameMenu.prototype = {
 
   // Add an option to menu
-  addMenuOption: function(text, callback) {
-    var txt = this.game.add.text(30, (this.optionCount * 80) + 150, text, style.navitem.default);
-    txt.inputEnabled = true;
-    txt.events.onInputUp.add(callback);
-    txt.events.onInputOver.add(function (target) {
-      target.setStyle(style.navitem.hover);
-    });
-    txt.events.onInputOut.add(function (target) {
-      target.setStyle(style.navitem.default);
-    });
-    this.optionCount ++;
-  },
+  // addMenuOption: function(text, callback) {
+  //   var txt = this.game.add.text(30, (this.optionCount * 80) + 150, text, style.navitem.default);
+  //   txt.inputEnabled = true;
+  //   txt.events.onInputUp.add(callback);
+  //   txt.events.onInputOver.add(function (target) {
+  //     target.setStyle(style.navitem.hover);
+  //   });
+  //   txt.events.onInputOut.add(function (target) {
+  //     target.setStyle(style.navitem.default);
+  //   });
+  //   this.optionCount ++;
+  // },
 
   // Initialize and set title
   init: function () {
@@ -36,20 +36,32 @@ GameMenu.prototype = {
     // set true to prevent game from pausing
     this.game.stage.disableVisibilityChange = false;
 
-    this.game.add.sprite(0, 0, 'menu-bg');
+    var menu = this.game.add.sprite(0, 0, 'menu-bg');
     this.game.add.existing(this.titleText);
 
-    this.addMenuOption('Start', function () {
-      self.game.state.start('Game');
-    });
-    this.addMenuOption('Multiplayer', function(){
-      self.game.state.start('Lobby');
-    })
-    this.addMenuOption('Options', function () {
-      console.log('You clicked Options!');
-    });
-    this.addMenuOption('Credits', function () {
-      console.log('You clicked Credits!');
-    });
+    var text = this.game.add.text(this.game.world.centerX, 400, "click to continue", style.navitem.default);
+    text.anchor.set(0.5);
+
+    text.alpha = 0;
+    this.game.add.tween(text).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+    menu.inputEnabled = true;
+    menu.events.onInputDown.add(function(){
+      this.game.state.start('Lobby');
+    }, this);
+
+
+    // this.addMenuOption('Start', function () {
+    //   self.game.state.start('Game');
+    // });
+    // this.addMenuOption('Multiplayer', function(){
+    //   self.game.state.start('Lobby');
+    // })
+    // this.addMenuOption('Options', function () {
+    //   console.log('You clicked Options!');
+    // });
+    // this.addMenuOption('Credits', function () {
+    //   console.log('You clicked Credits!');
+    // });
   }
 };
